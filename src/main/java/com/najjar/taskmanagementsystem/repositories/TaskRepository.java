fe6c6ea2f79cbf1ac0d1358bc320bb5c509454cb
najjariheb6 <najjariheb6@gmail.com>
2023-06-20 17:28:58 +0100

    update repos

--- src/main/java/com/najjar/taskmanagementsystem/repositories/TaskRepository.java
+++ src/main/java/com/najjar/taskmanagementsystem/repositories/TaskRepository.java
@@ -2,6 +2,8 @@ package com.najjar.taskmanagementsystem.repositories;
 
 import com.najjar.taskmanagementsystem.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
